4f2db85067e1cd7b8788c2defe04cd889200d834
Alexei Sergeenko <Odiozny@gmail.com>
2024-02-19 23:16:25 +0300

    test: BestContributorToSelenide

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+N
+src/test/java/TrivioTests.java,7/c/7cdafd074d4e49be7ee3bcf9859835c09d2fe897
+[
++src/test/java/SelenideRepositorySearch.java,5/f/5fa3ecac8e34f13c66f0d49a9bc39408cb5669b6
+\
+,src/test/java/BestContributorToSelenide.java,3/4/341a8eff4bba69b02cda170a491ace26213add4b
+N
+src/test/java/SearchTests.java,7/8/78f8069f986607d1e4dbcf7cd18426afdc528403
---
+++ src/test/java/BestContributorToSelenide.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BestContributorToSelenide {
+    @Test
+
+    void andreiSolntsevShoudBeTheFirstContributor() {
+        String browserSize = "400x400";
+
+        open("https://github.com/selenide/selenide");
+    $("div.Layout-sidebar").$(byText("Contributors"))
+                    .closest("h2").sibling(0).$$("li").first().hover();
+    $$(".Popover").findBy(visible).shouldHave(text("Andrei Solntsev"));
+    screenshot("screen");
+    }
+}
