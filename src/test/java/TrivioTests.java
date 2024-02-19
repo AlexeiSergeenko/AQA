b070eb8e8122ea8c55cff0566a6a5a11db32b024
Alexei Sergeenko <sergeenko@proton.me>
2024-02-19 17:18:18 +0300

    add TrivioTests

---
+++ src/test/java/TrivioTests.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TrivioTests {
+   @BeforeAll
+   static void beforeall() {
+       boolean holdBrowserOpen = true;
+       String browserSize = "1920x1080";
+       baseUrl = "https://login.trivio.ru/";
+//     Configuration.browser = "edge";
+   }
+   @Test
+   void successfulAithorisation( ) {
+       open(baseUrl);
+       $("#inputLogin").sendKeys("demo");
+       $("#inputPassword").sendKeys("demo");
+
+       $("#button_submit").click();
+   }
+}
