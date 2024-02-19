d9781f67319d6636d0430be86e05b0b7d85540e6
Alexei Sergeenko <Odiozny@gmail.com>
2024-02-19 23:26:44 +0300

    add BestContributorToSelenide

--- src/test/java/BestContributorToSelenide.java
+++ src/test/java/BestContributorToSelenide.java
@@ -8,7 +8,6 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class BestContributorToSelenide {
     @Test
-
     void andreiSolntsevShoudBeTheFirstContributor() {
         String browserSize = "400x400";
 
